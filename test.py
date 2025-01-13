a1d469537bb76b9962e9b0af1f5dfc8327fc7391
xcheng-tsinghua <786682191@qq.com>
2025-01-13 18:51:46 +0800

    change

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     # vis.vis_step_file(r'D:\document\DeepLearning\tmp\STEPMillion_pack1\00000038\00000038_c7d977f326364e35bb5b5d27_step_001.step')
 
     # img_proc.remove_png_white_pixel(r'E:\document\deeplearning_idea\参数化点云\参数化点云小论文\网站\cstnetwork.io\static\images\data_percentage.png')
-    # img_proc.remove_png_white_pixel_batched(r'E:\document\deeplearning_idea\参数化点云\桌面文件\fig\suppl')
+    img_proc.remove_png_white_pixel_batched(r'C:\Users\ChengXi\Desktop\sk_temporal')
 
     # 简单r'D:\document\DeepLearning\tmp\STEPMillion_pack1\00000029\00000029_ad34a3f60c4a4caa99646600_step_009.step'
 
-    vis.vis_data2d('log/loss-time.txt')
+    # vis.vis_data2d('log/loss-time.txt')
 
 
 
